1b5851d53956aeb635e3ed93ff952463add7eecf
MirkoMolteni <mirko.molteni.sc@gmail.com>
2024-02-03 10:39:19 +0100

    Migliorato funzionamento

--- src/main.py
+++ src/main.py
@@ -3,17 +3,29 @@ import matplotlib.pyplot as plt
 from key import AlphaVantageKEY
 
 def main():
+    url = "https://www.alphavantage.co/query"
     # symbol = input("Enter a stock symbol: ")
-    # response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey={AlphaVantageKEY}")
-    response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=demo")
+    # response = requests.get(url, params={
+    #     "function": "TIME_SERIES_DAILY",
+    #     "symbol": "IBM",
+    #     "outputsize": "full",
+    #     "apikey": AlphaVantageKEY
+    # })
+    response = requests.get(url, params={
+        "function": "TIME_SERIES_DAILY",
+        "symbol": "IBM",
+        "outputsize": "full",
+        "apikey": "demo"
+    })
     data = response.json()
     closing_prices = [float(data["Time Series (Daily)"][date]["4. close"]) for date in data["Time Series (Daily)"]]
     dates = list(data["Time Series (Daily)"].keys())
     plt.plot(dates, closing_prices)
+    # plt.title(f"{symbol} Stock Price")
     plt.title("IBM Stock Price")
     plt.xlabel("Date")
     plt.ylabel("Price")
-    # plt.xticks(rotation=45)
+    plt.xticks(rotation=45)
     plt.show()
     
     
